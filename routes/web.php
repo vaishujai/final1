b147cfcfe418b4e179e1d760181f1bafb138b3a1
Vaishnavi <vj222@njit.edu>
2018-12-15 20:35:49 -0500

    Feature: addding route and editing login and register.blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
+
+Route::get('/user/activation/{token}', 'Auth\RegisterController@userActivation');
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
